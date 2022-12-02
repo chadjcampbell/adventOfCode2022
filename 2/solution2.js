288555f0abd50d2b64dae1e8077a68840b6a7ae4
Chad <chadjcampbell@gmail.com>
2022-12-02 15:20:28 -0500

    part 1 done

--- 2/solution2.js
+++ 2/solution2.js
@@ -17,9 +17,30 @@ readTextFile1("./data2.txt");
 let splitData = data.split("\n");
 let totalPoints = 0;
 let abc = [0, 1, 2];
-let xyz = { X: 1, Y: 2, Z: 3 };
+let xyzPoints = { X: 1, Y: 2, Z: 3 };
+
+function roundPts(opponent, player) {
+  if (opponent === "A") {
+    if (player === "X") return 3;
+    if (player === "Y") return 6;
+    if (player === "Z") return 0;
+  }
+  if (opponent === "B") {
+    if (player === "X") return 0;
+    if (player === "Y") return 3;
+    if (player === "Z") return 6;
+  }
+  if (opponent === "C") {
+    if (player === "X") return 6;
+    if (player === "Y") return 0;
+    if (player === "Z") return 3;
+  }
+}
 
 for (let i = 0; i < splitData.length; i++) {
   let [opponent, player] = splitData[i].split(" ");
-  console.log(xyz[`${player}`]);
+  totalPoints += xyzPoints[`${player}`];
+  totalPoints += roundPts(opponent, player);
 }
+
+console.log(totalPoints);
