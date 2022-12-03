7f66e2d4829287e19037164bb4717730e10c634f
Chad <chadjcampbell@gmail.com>
2022-12-03 13:50:22 -0500

    part 1 done

--- 3/solution3.js
+++ 3/solution3.js
@@ -12,6 +12,29 @@ function readTextFile1(file) {
   rawFile.send(null);
 }
 
+function findItem(arr) {
+  const first = arr.slice(0, arr.length / 2);
+  const second = arr.slice(arr.length / 2);
+  const splitFirst = first.split("");
+  const splitSecond = second.split("");
+  const letter = splitFirst.filter((letter) => splitSecond.includes(letter));
+  return Array.from(new Set([...letter]))[0];
+}
+
 readTextFile1("./data3.txt");
 
 const splitData = data.split("\n");
+
+let points = 0;
+let totalPoints = 0;
+for (let i = 0; i < splitData.length; i++) {
+  let letter = findItem(splitData[i]);
+  if (letter == letter.toUpperCase()) {
+    points = letter.charCodeAt(0) - 38;
+  } else {
+    points = letter.charCodeAt(0) - 96;
+  }
+  totalPoints += points;
+}
+
+console.log(totalPoints);
