1bf5f7aa021ceee89d4d3b5ebbcb2fd76930199b
Chad Campbell <chadjcampbell@gmail.com>
2022-12-05 12:41:37 -0500

    hard coded stacks in object

--- 5/solution5.js
+++ 5/solution5.js
@@ -1,3 +1,5 @@
+const { Z_ASCII } = require("zlib");
+
 let data;
 function readTextFile1(file) {
   let rawFile = new XMLHttpRequest();
@@ -13,6 +15,18 @@ function readTextFile1(file) {
 }
 
 readTextFile1("./data4.txt");
+
+let stacks = {
+  1: ["F", "C", "P", "G", "Q", "R"],
+  2: ["W", "T", "C", "P"],
+  3: ["B", "H", "P", "M", "C"],
+  4: ["L", "T", "Q", "S", "M", "P", "R"],
+  5: ["P", "H", "J", "Z", "V", "G", "N"],
+  6: ["D", "P", "J"],
+  7: ["L", "G", "P", "Z", "F", "J", "T", "R"],
+  8: ["N", "L", "H", "C", "F", "P", "T", "J"],
+  9: ["G", "V", "Z", "Q", "H", "T", "C", "W"],
+};
 
 const splitData = data.split("\n");
 
