975c114f27ee22a866893d19e032c542649ca377
Chad Campbell <chadjcampbell@gmail.com>
2022-12-08 18:44:35 -0500

    part 1 work in progress

--- 8/solution8.js
+++ 8/solution8.js
@@ -14,4 +14,68 @@ function readTextFile1(file) {
 
 readTextFile1("./data8.txt");
 
-const splitData = data.split("\n");
+class TreeNode {
+  constructor(height) {
+    this.height = height;
+    this.visible = true;
+  }
+}
+
+const splitData = data.split("\r\n");
+let grid = splitData.map((line) => {
+  let nodes = line.split("");
+  return nodes.map((node) => {
+    if (!node.isNaN) {
+      return new TreeNode(parseInt(node));
+    }
+  });
+});
+
+let blocked = 0;
+for (let i = 0; i < grid.length; i++) {
+  const line = grid[i];
+  for (let j = 0; j < line.length; j++) {
+    const node = grid[i][j];
+    for (let x = 0; x < i; x++) {
+      if (grid[x][j].height >= node.height) {
+        blocked++;
+        break;
+      }
+    }
+    for (let x = j + 1; x < 100 - j; x++) {
+      if (grid[i][x].height >= node.height) {
+        blocked++;
+        break;
+      }
+    }
+    for (let x = i + 1; x < 100 - i; x++) {
+      if (grid[x][j].height >= node.height) {
+        blocked++;
+        break;
+      }
+    }
+    for (let x = 0; x < j; x++) {
+      if (grid[i][x].height >= node.height) {
+        blocked++;
+        break;
+      }
+    }
+    if (blocked === 4) {
+      node.visible = false;
+    }
+    blocked = 0;
+  }
+}
+
+let visibleTreeCount = 0;
+grid.forEach((line) => {
+  line.forEach((element) => {
+    if (element.visible === true) {
+      visibleTreeCount++;
+    }
+  });
+});
+
+console.log(visibleTreeCount);
+//9407 too high
+//7803 too high
