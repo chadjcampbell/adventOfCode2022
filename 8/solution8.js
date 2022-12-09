7b02a0ff623db81fd38045f411042b7d9accd418
Chad <chadjcampbell@gmail.com>
2022-12-08 23:35:12 -0500

    part 2 done

--- 8/solution8.js
+++ 8/solution8.js
@@ -18,6 +18,7 @@ class TreeNode {
   constructor(height) {
     this.height = height;
     this.visible = true;
+    this.scene = 0;
   }
 }
 
@@ -40,13 +41,13 @@ for (let i = 0; i < grid.length; i++) {
         break;
       }
     }
-    for (let x = j + 1; x < 99; x++) {
+    for (let x = j + 1; x < line.length; x++) {
       if (grid[i][x].height >= node.height) {
         blocked++;
         break;
       }
     }
-    for (let x = i + 1; x < 99; x++) {
+    for (let x = i + 1; x < line.length; x++) {
       if (grid[x][j].height >= node.height) {
         blocked++;
         break;
@@ -76,3 +77,51 @@ grid.forEach((line) => {
 
 //part1
 console.log(visibleTreeCount);
+
+for (let i = 0; i < grid.length; i++) {
+  const line = grid[i];
+  for (let j = 0; j < line.length; j++) {
+    let viewN = 0;
+    let viewE = 0;
+    let viewS = 0;
+    let viewW = 0;
+    const node = grid[i][j];
+    for (let x = i - 1; x >= 0; x--) {
+      viewN++;
+      if (grid[x][j].height >= node.height) {
+        break;
+      }
+    }
+    for (let x = j + 1; x < line.length; x++) {
+      viewE++;
+      if (grid[i][x].height >= node.height) {
+        break;
+      }
+    }
+    for (let x = i + 1; x < line.length; x++) {
+      viewS++;
+      if (grid[x][j].height >= node.height) {
+        break;
+      }
+    }
+    for (let x = j - 1; x >= 0; x--) {
+      viewW++;
+      if (grid[i][x].height >= node.height) {
+        break;
+      }
+    }
+    node.scene = viewN * viewE * viewS * viewW;
+  }
+}
+
+let highScore = 0;
+grid.forEach((line) => {
+  line.forEach((element) => {
+    if (element.scene > highScore) {
+      highScore = element.scene;
+    }
+  });
+});
+
+//part2
+console.log(highScore);
