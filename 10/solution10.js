debbf03d620e99092c257c3ad92a9501421e3f02
Chad <chadjcampbell@gmail.com>
2022-12-10 17:29:15 -0500

    encapsulate part 1

--- 10/solution10.js
+++ 10/solution10.js
@@ -20,32 +20,34 @@ const instructions = splitData.map((line) => {
   return line.split(" ");
 });
 
-function updateX() {
-  if (cycle === 20) {
-    signalStrength += cycle * X;
-  } else if ((cycle - 20) % 40 === 0) {
-    signalStrength += cycle * X;
+function part1() {
+  function updateX() {
+    if (cycle === 20) {
+      signalStrength += cycle * X;
+    } else if ((cycle - 20) % 40 === 0) {
+      signalStrength += cycle * X;
+    }
   }
-}
-
-let cycle = 0;
-let X = 1;
-let signalStrength = 0;
-for (let i = 0; i < instructions.length; i++) {
-  let action = instructions[i][0];
-  let value = parseInt(instructions[i][1]);
-  if (action === "noop") {
-    cycle++;
-    updateX();
+  let cycle = 0;
+  let X = 1;
+  let signalStrength = 0;
+  for (let i = 0; i < instructions.length; i++) {
+    let action = instructions[i][0];
+    let value = parseInt(instructions[i][1]);
+    if (action === "noop") {
+      cycle++;
+      updateX();
+    }
+    if (action === "addx") {
+      cycle++;
+      updateX();
+      cycle++;
+      updateX();
+      X += value;
+    }
   }
-  if (action === "addx") {
-    cycle++;
-    updateX();
-    cycle++;
-    updateX();
-    X += value;
-  }
+  console.log(signalStrength);
 }
 
 //part1
-console.log(signalStrength);
+part1();
