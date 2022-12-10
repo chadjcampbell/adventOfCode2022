4190dce777e4d5c3ea59baaf027775ccce19dcd4
Chad <chadjcampbell@gmail.com>
2022-12-09 20:45:45 -0500

    part 1 done

--- 9/solution9.js
+++ 9/solution9.js
@@ -12,35 +12,71 @@ function readTextFile1(file) {
   rawFile.send(null);
 }
 
-readTextFile1("./example.txt");
+readTextFile1("./data9.txt");
 
-const splitData = data.split("\r\n");
+const splitData = data.split("\n");
 
 const instructions = splitData.map((line) => {
   return line.split(" ");
 });
 
-function checkTail(headCoords) {
-  const headGrid = makeHeadGrid(headCoords);
-  if (headGrid.includes(currentTailCoords)) {
-    return;
+function checkTail() {
+  let headGrid = makeHeadGrid();
+  if (headGrid.some((a) => currentTailCoords.every((v, i) => v === a[i]))) {
+    return false;
   } else {
-    pullTail();
+    return true;
   }
 }
 
-function makeHeadGrid(headCoords) {
+function makeHeadGrid() {
   let headGrid = [];
   for (let i = -1; i < 2; i++) {
     for (let j = -1; j < 2; j++) {
-      headGrid.push([headCoords[0] + i, headCoords[1] + j]);
+      headGrid.push([currentHeadCoords[0] + i, currentHeadCoords[1] + j]);
     }
   }
   return headGrid;
 }
 
-function pullTail() {
-  return;
+function pullTail(direction) {
+  switch (direction) {
+    case "U":
+      if (currentHeadCoords[1] !== currentTailCoords[1]) {
+        currentTailCoords = [currentHeadCoords[0] + 1, currentHeadCoords[1]];
+        break;
+      }
+      currentTailCoords[0] -= 1;
+      break;
+    case "R":
+      if (currentHeadCoords[0] !== currentTailCoords[0]) {
+        currentTailCoords = [currentHeadCoords[0], currentHeadCoords[1] - 1];
+        break;
+      }
+      currentTailCoords[1] += 1;
+      break;
+    case "D":
+      if (currentHeadCoords[1] !== currentTailCoords[1]) {
+        currentTailCoords = [currentHeadCoords[0] - 1, currentHeadCoords[1]];
+        break;
+      }
+      currentTailCoords[0] += 1;
+      break;
+    case "L":
+      if (currentHeadCoords[0] !== currentTailCoords[0]) {
+        currentTailCoords = [currentHeadCoords[0], currentHeadCoords[1] + 1];
+        break;
+      }
+      currentTailCoords[1] -= 1;
+      break;
+  }
+  if (
+    !tailVisitedCoords.some((a) =>
+      currentTailCoords.every((v, i) => v === a[i])
+    )
+  ) {
+    tailVisitedCoords.push([currentTailCoords[0], currentTailCoords[1]]);
+  }
 }
 
 let currentHeadCoords = [0, 0];
@@ -65,6 +101,8 @@ for (let i = 0; i < instructions.length; i++) {
         currentHeadCoords[1] -= 1;
         break;
     }
-    checkTail(currentHeadCoords);
+    if (checkTail()) pullTail(direction);
   }
 }
+
+console.log(tailVisitedCoords.length);
