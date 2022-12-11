111194c52f11a9d8fa2f47f83ec9439f3c91c418
Chad <chadjcampbell@gmail.com>
2022-12-11 10:31:30 -0500

    set up problem

---
+++ 11/solution11.js
@@ -0,0 +1,17 @@
+let data;
+function readTextFile1(file) {
+  let rawFile = new XMLHttpRequest();
+  rawFile.open("GET", file, false);
+  rawFile.onreadystatechange = function () {
+    if (rawFile.readyState === 4) {
+      if (rawFile.status === 200 || rawFile.status == 0) {
+        data = rawFile.responseText;
+      }
+    }
+  };
+  rawFile.send(null);
+}
+
+readTextFile1("./data11.txt");
+
+const splitData = data.split("\n");
