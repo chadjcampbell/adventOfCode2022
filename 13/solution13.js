ea1a3b6f47794dcdef76616d016536bbc3d36e5a
Chad Campbell <chadjcampbell@gmail.com>
2022-12-13 15:50:29 -0500

    part 1 WIP

--- 13/solution13.js
+++ 13/solution13.js
@@ -12,6 +12,46 @@ function readTextFile1(file) {
   rawFile.send(null);
 }
 
-readTextFile1("./data13.txt");
+readTextFile1("./example.txt");
 
-const splitData = data.split("\r\n");
+const splitData = data.split("\r\n\r\n");
+
+let index = 1;
+let sumIndex = 0;
+
+function inOrder(left, right) {
+  if (left === undefined) return true;
+  if (right === undefined) return false;
+  if (typeof left === "number" && typeof right === "number") {
+    if (left < right) return true;
+    if (left > right) return false;
+  }
+  if (typeof left === "object" && typeof right === "object") {
+    for (let i = 0; i < right.length; i++) {
+      if (inOrder(left[i], right[i]) === undefined) {
+        continue;
+      } else {
+        return inOrder(left[i], right[i]);
+      }
+    }
+  }
+  if (typeof left === "number" && typeof right !== "number") {
+    return inOrder([left], right);
+  }
+  if (typeof right === "number" && typeof left !== "number") {
+    return inOrder(left, [right]);
+  }
+}
+
+for (let i = 0; i < splitData.length; i++) {
+  const packet = splitData[i].split("\r\n");
+  const leftPacket = JSON.parse(packet[0]);
+  const rightPacket = JSON.parse(packet[1]);
+  if (inOrder(leftPacket, rightPacket)) {
+    sumIndex += index;
+  }
+  index += 1;
+}
+
+console.log(sumIndex);
+//7303 too high
