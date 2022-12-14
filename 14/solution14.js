c8e31fdd1b0288394e7b938e42389f8cb9727f83
Chad <chadjcampbell@gmail.com>
2022-12-14 12:08:06 -0500

    set up problem 14

---
+++ 14/solution14.js
@@ -0,0 +1,18 @@
+let data;
+function readTextFile1(file) {
+  let rawFile = new XMLHttpRequest();
+  rawFile.open("GET", file, false);
+  rawFile.onreadystatechange = function () {
+    if (rawFile.readyState === 4) {
+      if (rawFile.status === 200 || rawFile.status == 0) {
+        data = rawFile.responseText;
+      }
+    }
+  };
+  rawFile.send(null);
+}
+
+readTextFile1("./data14.txt");
+
+const splitData = data.split("\n");
+console.log(splitData);
